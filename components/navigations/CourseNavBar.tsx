4b500a2bf3486c382341f55f1deec0b9649fa4ba
Hisam Fahri <me@hisamafahri.com>
2022-05-19 05:19:14 +0700

    feat(editor): add content container and bottom bar

--- components/navigations/CourseNavBar.tsx
+++ components/navigations/CourseNavBar.tsx
@@ -1,46 +1,40 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import { FiHelpCircle } from "react-icons/fi";
 
 export default function CourseNavBar() {
   return (
-    <div className="navbar bg-primary">
+    <div className="navbar bg-primary fixed top-0">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl text-white font-ng-mono">
-          g_&gt;
-        </a>
-        <a className="btn btn-ghost normal-case text-md font-normal text-white font-ng-text">
-          Beranda
-        </a>
-        <a className="btn btn-ghost normal-case text-md font-normal text-white font-ng-text">
-          Menu
-        </a>
+        <Link href="/">
+          <a className="btn btn-ghost normal-case text-xl text-white font-ng-mono">
+            g_&gt;
+          </a>
+        </Link>
+        <Link href="#">
+          <a className="btn btn-ghost normal-case text-md font-normal text-white font-ng-text">
+            Beranda
+          </a>
+        </Link>
+        <Link href="#">
+          <a className="btn btn-ghost normal-case text-md font-normal text-white font-ng-text">
+            Menu
+          </a>
+        </Link>
       </div>
 
       <div className="flex-none">
         <ul className="menu menu-horizontal p-0">
           <li>
-            <a className="btn btn-ghost normal-case text-md font-normal text-white font-ng-text">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="feather feather-help-circle"
-              >
-                <circle cx="12" cy="12" r="10" />
-                <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
-                <line x1="12" y1="17" x2="12.01" y2="17" />
-              </svg>
-              Dapatkan Bantuan
-            </a>
+            <Link href="#">
+              <a className="btn btn-outline normal-case text-md font-normal text-white font-ng-text hover:bg-white hover:text-primary">
+                <FiHelpCircle />
+                Dapatkan Bantuan
+              </a>
+            </Link>
           </li>
         </ul>
       </div>
     </div>
   );
-};
-
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import CourseNavBar from "../components/navigations/CourseNavBar";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -13,9 +14,48 @@ const Home: NextPage = () => {
 
       <main>
         <CourseNavBar />
+        <CourseContent />
+        <BottomBar />
       </main>
     </div>
   );
 };
 
 export default Home;
+
+function CourseContent() {
+  return (
+    // Padding vertical 64px (py-16) is used to make sure the content is not
+    // hidden behind navbar and/or bottom bar
+    <div className="flex flex-1 flex-row w-full h-screen py-16"> 
+      <div className="w-1/3">
+        <p>1</p>
+      </div>
+      <div className="w-1/3">
+        <p>2</p>
+      </div>
+      <div className="w-1/3">
+        <p>3</p>
+      </div>
+    </div>
+  );
+}
+
+function BottomBar() {
+  return (
+    <div className="navbar bg-primary bottom-0 fixed">
+      <div className="flex-1 justify-end">
+        <Link href="#">
+          <a className="btn btn-ghost normal-case text-md font-normal text-white font-ng-text">
+            Sebelumnya
+          </a>
+        </Link>
+        <Link href="#">
+          <a className="btn btn-outline normal-case text-md font-normal text-white font-ng-text hover:bg-white hover:text-primary">
+            Selanjutnya
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+}
