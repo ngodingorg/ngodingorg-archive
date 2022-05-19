a1158824548d7a9ef8c57a85fb6ce2f2b7e426f0
Hisam Fahri <me@hisamafahri.com>
2022-05-19 20:01:07 +0700

    fix(bottom): add white border

--- components/navigations/BottomBar.tsx
+++ components/navigations/BottomBar.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default function BottomBar() {
   return (
-    <div className="navbar bg-primary bottom-0 fixed">
+    <div className="navbar bg-primary bottom-0 fixed border-t border-white">
       <div className="flex-1 justify-end">
         <Link href="#">
           <a className="btn btn-ghost normal-case text-md font-normal text-white font-ng-text">
