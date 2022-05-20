08a2df7003b625af8ceca0d982c9fc51404a9324
Hisam Fahri <me@hisamafahri.com>
2022-05-20 19:17:09 +0700

    feat(editor): add bottom action bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import CourseNavBar from "../components/navigations/CourseNavBar";
 import BottomBar from "../components/navigations/BottomBar";
 import matter from "gray-matter";
-import { FiBookOpen, FiGlobe } from "react-icons/fi";
+import { FiBookOpen, FiGlobe, FiRefreshCcw } from "react-icons/fi";
 import Guide from "../components/course/Guide";
 import Editor from "@monaco-editor/react";
 import { useEffect, useState } from "react";
@@ -47,6 +47,11 @@ function CourseContent({ content }: any) {
     setEditorContent(value);
   }
 
+  function resetCode() {
+    setEditorContent(defaultValue);
+    setSrcDoc(`<html><body>${defaultValue}</body></html>`);
+  }
+
   return (
     // Padding vertical 64px (py-16) is used to make sure the content is not
     // hidden behind navbar and/or bottom bar
@@ -83,20 +88,45 @@ function CourseContent({ content }: any) {
           path="index.html"
           defaultLanguage="html"
           defaultValue={defaultValue}
+          value={editorContent}
           onChange={handleEditorChange}
         />
+        <div
+          className="bg-ng-vs-code-secondary flex flex-row items-center fixed bottom-16 w-1/3"
+          style={{ marginTop: 1 }}
+        >
+          <div className="tooltip" data-tip="Periksa jawaban yang kamu berikan">
+            <button className="font-ng-mono font-semibold bg-yellow-400 py-3 px-6">
+              Periksa
+            </button>
+          </div>
+          <div
+            className="tooltip"
+            style={{ color: "white" }}
+            data-tip="Atur ulang kode"
+          >
+            <button
+              onClick={resetCode}
+              className="font-ng-mono font-semibold py-4 px-4 h-full"
+            >
+              <FiRefreshCcw />
+            </button>
+          </div>
+        </div>
       </div>
       <div className="w-1/3">
         <div
-          className="bg-white flex flex-row items-center fixed top-16 w-1/3"
+          className="bg-slate-100 flex flex-row items-center fixed top-16 w-1/3"
           style={{ marginTop: 1 }}
         >
-          <div className="bg-slate-100 p-4" style={{ color: "#94a3b8" }}>
+          <div className="p-4" style={{ color: "#94a3b8" }}>
             <FiGlobe />
           </div>
-          <p className="font-ng-text text-sm font-normal ml-4">
-            http://localhost:8000
-          </p>
+          <div className="bg-white py-3.5 pl-4 pr-12">
+            <p className="font-ng-text text-sm font-normal">
+              http://localhost:8000
+            </p>
+          </div>
         </div>
         <iframe
           title="output"
