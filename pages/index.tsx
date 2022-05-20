f963ab6d31f746dc417b4a5b00ada48897b7b6dc
Hisam Fahri <me@hisamafahri.com>
2022-05-20 16:59:53 +0700

    perf(editor): set delay before render input code

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import matter from "gray-matter";
 import { FiBookOpen, FiGlobe } from "react-icons/fi";
 import Guide from "../components/course/Guide";
 import Editor from "@monaco-editor/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const defaultValue = "<p>Halooo</p>";
 
@@ -32,9 +32,21 @@ export default Home;
 
 function CourseContent({ content }: any) {
   const [editorContent, setEditorContent] = useState(defaultValue);
+  const [srcDoc, setSrcDoc] = useState(defaultValue);
+
+  // Give 0.5 second delay before rendering
+  useEffect(() => {
+    const delayOnRender = setTimeout(() => {
+      setSrcDoc(`<html><body>${editorContent}</body></html>`);
+    }, 500);
+
+    return () => clearTimeout(delayOnRender);
+  }, [editorContent]);
+
   function handleEditorChange(value: any) {
     setEditorContent(value);
   }
+
   return (
     // Padding vertical 64px (py-16) is used to make sure the content is not
     // hidden behind navbar and/or bottom bar
@@ -79,10 +91,12 @@ function CourseContent({ content }: any) {
           className="bg-white flex flex-row items-center fixed top-16 w-1/3"
           style={{ marginTop: 1 }}
         >
-        <div className="bg-slate-100 p-4" style={{color: "#94a3b8"}}>
-          <FiGlobe />
-        </div>
-          <p className="font-ng-text text-sm font-normal ml-4">http://localhost:8000</p>
+          <div className="bg-slate-100 p-4" style={{ color: "#94a3b8" }}>
+            <FiGlobe />
+          </div>
+          <p className="font-ng-text text-sm font-normal ml-4">
+            http://localhost:8000
+          </p>
         </div>
         <iframe
           title="output"
@@ -91,7 +105,7 @@ function CourseContent({ content }: any) {
           width="100%"
           className="pt-16"
           frameBorder="0"
-          srcDoc={`<html><body>${editorContent}</body></html>`}
+          srcDoc={srcDoc}
         />
       </div>
     </div>
