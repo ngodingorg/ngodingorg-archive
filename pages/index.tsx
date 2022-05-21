1db0a5f8143d66b8e454a61e1055cd24a481a953
Hisam Fahri <me@hisamafahri.com>
2022-05-21 19:15:49 +0700

    feat(home): add link to current development

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -11,7 +12,9 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <p>Home page</p>
+        <Link href="/course/html/1">
+          <p className="underline font-ng-mono cursor-pointer">Go to example course</p>
+        </Link>
       </main>
     </div>
   );
