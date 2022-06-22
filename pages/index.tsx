26bd0de51b657b20af229b08a5bbe2d5f9dd1039
Hisam Fahri <hisamafahri@gmail.com>
2022-06-22 10:16:54 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>ngoding.org</title>
+        <title>test changes - ngoding.org</title>
         <meta name="description" content="ngoding.org" />
         <link rel="icon" href="/images/favicon.ico" />
       </Head>
